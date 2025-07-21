4227c2b50dd5a023c586b38e782f209a7b74348a
Pigritiae <Bernardo.calabria.programacao@gmail.com>
2025-07-21 15:34:21 -0300

    Added 8x8 Pixel Art files(21/07, from "igormelol"s repository)

---
+++ Folder B 95/Pixel Art.js
@@ -0,0 +1,25 @@
+const grid = document.getElementById('grid');
+const colorPicker = document.getElementById('color');
+const limparBtn = document.getElementById('limpar');
+
+function criarGrid() {
+  for (let i = 0; i < 64; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+
+    cell.addEventListener('click', () => {
+      cell.style.backgroundColor = colorPicker.value;
+    });
+
+    grid.appendChild(cell);
+  }
+}
+
+limparBtn.addEventListener('click', () => {
+  document.querySelectorAll('.cell').forEach(cell => {
+    cell.style.backgroundColor = '#fff';
+  });
+});
+
+criarGrid();
+/* Código Copiado do repositório de GitHub do usuário igormelol */
